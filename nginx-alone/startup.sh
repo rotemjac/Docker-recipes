1253f9f7d9ad9cb2ac2b00429df50348d37ed835
Rotem Jackoby <rotem_jac@yahoo.com>
2019-09-26 00:41:26 +0300

    Remove irrelevant code

--- nginx-alone/startup.sh
+++ nginx-alone/startup.sh
@@ -16,30 +16,3 @@ $TEMPLATE_DIR:$TEMPLATE_REMOTE_DIR $IMAGE_NAME \
 
 
 
-kernel-panic-exploits
-container-breakouts-privilege-escalation
-poisoned-images
-denial-of-service-attacks
-compromising-secrets
-application-level-threats
-host-system-level-treats
-security-by-design
-setuidsetgid
-controlling-cpu-usage
-controlling-memory-usage
-verifying-images
-set-container-filesystem-to-read-only
-set-a-user
-do-not-use-environment-variables-to-share-secrets
-use-orchestrators-secret-managers
-do-not-run-containers-in-the-privileged-mode
-turn-off-inter-container-communication
-set-volumes-to-read-only
-only-install-necessary-packages
-make-sure-docker-is-up-to-date
-use-vulnerability-analysis-scanners
-properly-configure-your-docker-registry-access-control
-security-through-obscurity
-secure-and-control-your-code
-use-limited-linux-capabilities
-use-seccomp
